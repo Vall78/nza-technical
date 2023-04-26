77910f2b41824c47682e1b3584e435e2224201e9
Vall78 <131019841+Vall78@users.noreply.github.com>
2023-04-26 23:01:23 +0700

    Update app.py

--- aim-technical/M7S1/pengenalan/app.py
+++ aim-technical/M7S1/pengenalan/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Belajar Flask Dasar"
 
 if __name__ == '__main__':
+    run_with_ngrok(app)
     app.run(port=5000, debug=True)
